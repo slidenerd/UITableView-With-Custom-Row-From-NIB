e466a3c77517dbac85e2d6eb674d4b6fd74bc66f
Vivek Ramesh <vivz@Vivz.local>
2015-05-12 22:03:58 +0530

    And BAM...that is how you make a UITableView with custom cells from a NIB file in Xcode

---
+++ CustomTableFromNIB/NameAndColorCell.swift
@@ -0,0 +1,14 @@
+//
+//  NameAndColorCell.swift
+//  CustomTableFromNIB
+//
+//  Created by Vivek Ramesh on 12/05/15.
+//  Copyright (c) 2015 slidenerd. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class NameAndColorCell : UITableViewCell {
+    @IBOutlet var nameLabel : UILabel!;
+    @IBOutlet var colorLabel : UILabel!;
+}
--- CustomTableFromNIB/ViewController.swift
+++ CustomTableFromNIB/ViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let nib = UINib(nibName: "NameAndColorCell", bundle: nil);
+        tableView.registerNib(nib, forCellReuseIdentifier: "MyTable")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +23,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100;
+    }
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 80;
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MyTable") as! NameAndColorCell
+        cell.nameLabel.text = "Vivz is now ranking at \(indexPath.row+1)"
+        cell.colorLabel.text = "Anky is at \(indexPath.row * 2)"
+        return cell;
+    }
 
 }
 
